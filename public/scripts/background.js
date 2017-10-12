7551cc47d2a421537b9829526ee086e22025732f
DUKE327 <32682488+DUKE327@users.noreply.github.com>
2017-10-12 14:34:28 -0700

    Add files via upload

---
+++ public/scripts/background.js
@@ -0,0 +1,22 @@
+var counter = 0;
+function changeBG(){
+    var imgs = [
+        "url(https://images.unsplash.com/photo-1422360902398-0a91ff2c1a1f?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
+        "url(https://images.unsplash.com/photo-1429041966141-44d228a42775?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
+        "url(https://images.unsplash.com/photo-1442551382982-e59a4efb3c86?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
+        "url(https://images.unsplash.com/photo-1422393462206-207b0fbd8d6b?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
+        "url(https://images.unsplash.com/photo-1470217957101-da7150b9b681?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
+        "url(https://images.unsplash.com/photo-1441794016917-7b6933969960?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
+        "url(https://images.unsplash.com/photo-1445264918150-66a2371142a2?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)",
+        "url(https://images.unsplash.com/photo-1488034976201-ffbaa99cbf5c?dpr=2&fit=crop&fm=jpg&h=825&ixlib=rb-0.3.5&q=50&w=1450)"
+      ]
+    
+    if(counter === imgs.length) counter = 0;
+    $("body").css("background-image", imgs[counter]);
+
+    counter++;
+}
+  
+  setInterval(changeBG, 2500);
+
+
